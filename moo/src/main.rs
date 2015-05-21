6aa94c16f40ccd8180684e1e58b401d371fefad3
Bas Pennings <baspster@gmail.com>
2015-05-21 12:10:28 +0200

    Basic experiments with moo value

---
+++ moo/src/main.rs
@@ -0,0 +1,33 @@
+#[derive(Debug)]
+enum Val {
+	Int(i32),
+	Float(f64),
+	Str(String),
+	List(Vec<Val>),
+	Error(String),
+	Obj(i32),
+}
+
+fn main() {
+	let v1 = Val::Int(123);
+	
+	let v2 = Val::Str("foo".to_string());
+	
+	let v3 = Val::List(vec![
+		Val::Int(1), 
+		Val::Str("foo".to_string()), 
+		Val::Int(3)
+	]);
+
+	let v4 = Val::Float(1.23);
+
+	let v5 = Val::Obj(456);
+
+	let v6 = Val::Error("INVARG".to_string());
+
+	let vals = vec![v1, v2, v3, v4, v5, v6];
+	
+	for v in vals {
+		println!("{:?}", v);
+	}
+}
